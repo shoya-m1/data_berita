c59e19225d6ab82fc3c486b1c2e005dab3883d4a
shoya <ilmimusthofa8189@gmail.com>
2025-05-16 15:39:47 +0700

    update slug search gempa

--- controller/scraper.js
+++ controller/scraper.js
@@ -5,9 +5,11 @@ module.exports = {
   newArtikels: async (req, res) => {
     const page = req.query.page;
     // const url = `https://cekfakta.kompas.com/${page == null ? 1 : page}`;
-    const url = `https://www.detik.com/search/searchnews?query=bencana%20alam&page=${
-      page == null ? 1 : page
-    }&result_type=relevansi`;
+    const url = `https://www.detik.com/search/searchnews?query=gempa&page=${page == null ? 1 : page}&result_type=relevansi&fromdatex=31/05/2020&todatex=31/05/2024`;
+    // const url = `https://www.detik.com/search/searchnews?query=bencana%20alam&page=${
+    //   page == null ? 1 : page
+    // }&result_type=relevansi`;
+
     const resp = await fetch(url);
     // console.log(resp);
     try {
